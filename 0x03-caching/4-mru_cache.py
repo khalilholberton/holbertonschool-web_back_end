ef704be32abd53827d7e5ffdfb3ac0007ef5e42f
khalilholberton <1071@holbertonschool.com>
2020-10-26 22:38:22 +0000

    fix

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+cashing system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    class MRUCache that inherits from BasicCashing
+    """
+
+    def __init__(self):
+        """
+        init method
+        """
+        super().__init__()
+        self.disip = []
+
+    def get(self, key):
+        """
+        return the value of key
+        """
+        if key in self.cache_data:
+            self.disip.remove(key)
+            self.disip.append(key)
+            return self.cache_data[key]
+        return None
+
+    def put(self, key, item):
+        """
+        this is the function put
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.disip.remove(key)
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    del self.cache_data[self.disip[-1]]
+                    print("DISCARD:", self.disip[-1])
+                    self.disip.pop(-1)
+                self.cache_data[key] = item
+            self.disip.append(key)
