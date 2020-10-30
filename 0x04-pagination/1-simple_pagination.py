2e5ce562063dd3f1239bb495d4c7261c2d513bb1
khalilholberton <1071@holbertonschool.com>
2020-10-30 21:35:51 +0000

    fix

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -38,7 +38,7 @@ class Server:
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """index range"""
-        a = page * page_size - page_size
-        b = a + page_size
-        return (a, b)
+    """index range"""
+    a = page * page_size - page_size
+    b = a + page_size
+    return (a, b)
