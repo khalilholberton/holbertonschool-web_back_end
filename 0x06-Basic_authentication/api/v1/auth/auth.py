1cbcfea7ee75e57b71a15e9ab6283208940c6d90
khalilholberton <1071@holbertonschool.com>
2020-11-10 22:52:43 +0000

    fix

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,19 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ def require_auth
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path.endswith("/"):
+            pass
+        else:
+            path = path + "/"
+        if excluded_paths[-1] != "/":
+            excluded_paths += "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ def authorization_header
