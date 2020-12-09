b263a05dec8f1958caa91910c00e6701804ababd
khalilholberton <1071@holbertonschool.com>
2020-12-09 16:05:18 +0000

    fix

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Simple Flask app
+"""
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """
+    return 0-index template
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
