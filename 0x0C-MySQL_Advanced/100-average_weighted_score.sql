f22cb8f1e1206ee8e3e93d5ec13d75911b2029bb
khalilholberton <1071@holbertonschool.com>
2021-01-23 20:59:18 +0000

    fix

---
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,14 @@
+-- procedure
+--
+delimiter ??
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+    ( IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT sum((SELECT weight
+		      	 FROM projects WHERE corrections.project_id = id) * score) / (SELECT sum(weight)
+		      			    	   		 FROM projects)
+		      			    	   		 FROM corrections WHERE corrections.user_id = user_id)
+    WHERE
+        users.id = user_id;
+END??
