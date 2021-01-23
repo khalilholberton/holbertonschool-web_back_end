bdd29ee7c6493aaadb448fb8a86aa9008cdcc1df
khalilholberton <1071@holbertonschool.com>
2021-01-23 20:35:04 +0000

    fix

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- function SaveDiv
+delimiter ??
+CREATE FUNCTION SafeDiv (
+       a INT,
+       b INT)
+RETURNS FLOAT
+BEGIN
+	IF b = 0 THEN
+	   RETURN 0;
+	END IF;
+
+	   RETURN (a / b);
+END??
