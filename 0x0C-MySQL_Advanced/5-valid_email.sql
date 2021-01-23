c501b6916930a7b2cb680d8e75a290918d2d9b31
khalilholberton <1071@holbertonschool.com>
2021-01-23 17:34:47 +0000

    fix

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Creates a trigger
+DELIMITER ??
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+??
