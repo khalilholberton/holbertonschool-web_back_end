d4a67e51122a4fbe4b472f46c55726b8aa5b7386
khalilholberton <1071@holbertonschool.com>
2021-01-26 19:48:06 +0000

    fix

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+schools_by_topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    schools_by_topic returns the list of school having a specific topic
+    """
+
+    return mongo_collection.find({'topics': topic})
