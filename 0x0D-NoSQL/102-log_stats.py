13eedef610aaec9e000245e177f9d5cedc2814e6
khalilholberton <1071@holbertonschool.com>
2021-01-26 20:50:31 +0000

    fix

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-"""
-nginx_log
-"""
+"""nginx_log"""
 from pymongo import MongoClient
 
-
-def nginx_log():
-    """ adding the top 10 of the most present IPs """
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     number = nginx_collection.count()
@@ -20,10 +16,7 @@ def nginx_log():
         {"method": "GET", "path": "/status"}).count()
     num_ips = nginx_collection.aggregate([
             {"$group": {"_id": "$ip", "total": {"$sum": 1}}},
-            {"$sort": {"total": -1}},
-            {"$limit": 10}
-        ])
-
+            {"$sort": {"total": -1}},{"$limit": 10}])
     print("{} logs".format(number))
     print("Methods:")
     print("\tmethod GET: {}".format(num_get))
@@ -35,7 +28,3 @@ def nginx_log():
     print("IPs:")
     for ip in num_ips:
         print("\t{}: {}".format(ip.get("_id"), ip.get("total")))
-
-
-if __name__ == "__main__":
-    nginx_log()
