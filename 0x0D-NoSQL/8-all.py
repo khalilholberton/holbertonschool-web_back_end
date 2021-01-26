a6bee2b34524a1f1b67557e1d0b81d7e56dcda20
khalilholberton <1071@holbertonschool.com>
2021-01-26 18:41:27 +0000

    fix

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -2,13 +2,14 @@
 """
 This module contains the func list_all
 """
+import pymongo
 
 
 def list_all(mongo_collection):
     """
     list all documents in the collection mymongo
     """
+    if not mongo_collection:
+        return []
     all_doc = mongo_collection.find()
-    if all_doc:
-        return all_doc
-    return []
+    return [d for d in all_doc]
