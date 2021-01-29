18c57c79001ffb2f521a0b5823c9c04ae3fb1672
khalilholberton <1071@holbertonschool.com>
2021-01-29 22:10:29 +0000

    fix

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,10 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  /* eslint-disable guard-for-in */
-  for (const idx in array) {
-    /* eslint-disable no-param-reassign */
-    const value = array[idx];
-    array[idx] = appendString + value;
+  const s = [];
+  for (const value of array) {
+    s.push(`${appendString}${value}`);
   }
 
-  return array;
+  return s;
 }
