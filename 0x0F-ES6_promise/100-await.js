4fa6d20e7857339dca23153886db260653fae125
khalilholberton <1071@holbertonschool.com>
2021-01-30 21:51:14 +0000

    fix

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -1,14 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  try {
-    const user_v = await createUser();
-    const photo_v = await uploadPhoto();
+  let usr;
+  let image;
 
+  try {
+    usr = await createUser();
+    image = await uploadPhoto();
   } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    }
-    return Promise.resolve({ photo_v, user_v });
+    photoval = null;
+    userval = null;
+  }
+  return { photo: image, user: usr };
 }
