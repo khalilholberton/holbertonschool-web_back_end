d888c4b3caf95d8aa43bb4e5d82509b94cbbb698
khalilholberton <1071@holbertonschool.com>
2021-02-02 12:43:43 +0000

    fix

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const objs = new Map();
+  objs.set('Apples', 10);
+  objs.set('Tomatoes', 10);
+  objs.set('Pasta', 1);
+  objs.set('Rice', 1);
+  objs.set('Banana', 5);
+
+  return objs;
+}
