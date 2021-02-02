777eb28e68a50e134ff7d68588fde263b63544ab
khalilholberton <1071@holbertonschool.com>
2021-02-02 19:40:38 +0000

    fix

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,20 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() { return this._amount; }
+
+  set amount(givenAmount) { this._amount = givenAmount; }
+
+  get currency() { return this._currency; }
+
+  set currency(givenCurrency) { this._currency = givenCurrency; }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) { return amount * conversionRate; }
+}
