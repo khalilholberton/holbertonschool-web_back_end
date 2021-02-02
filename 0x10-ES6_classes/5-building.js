02e6ffbece418cd31b6b1f8cbb17c2650745fee9
khalilholberton <1071@holbertonschool.com>
2021-02-02 19:55:04 +0000

    fix

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,10 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage === 'undefined') throw new Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
+  }
+
+  get sqft() { return this._sqft; }
+
+  set sqft(givenSqft) { this._sqft = givenSqft; }
+}
